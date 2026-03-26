4419b7b8be6318c2c7ce6b62ca3906a2f3ee5967
Naseem Qurbanli <nq222af@student.lnu.se>
2026-03-26 22:54:58 +0100

    New function finding duplicates

--- Problems/Arrays/easy.java
+++ Problems/Arrays/easy.java
@@ -16,6 +16,7 @@ public class easy {
         int[] result =  e.pairTarget(nums, target);
         System.out.println(Arrays.toString(result));
         System.out.println("max vinst: " + e.MaxValue(nums));
+        e.Duplicate("abcabcaa");
     }
 
     /* Finding the target by keypair value */
@@ -67,5 +68,44 @@ public class easy {
 
 
 
+    public int[] solve(int[] nums) {
+        int n = nums.length;
+        int [] result = new int[n];
 
+        result[0] = 1;
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= right;
+            right *= nums[i];
+        }
+
+        return result;
+    }
+
+    /**
+     * Finding duplicates characters and counter
+     */
+
+    public void Duplicate(String s) {
+        
+        char [] arr = s.toCharArray();
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length;) {
+            int count = 1;
+            while(i + count < arr.length && arr [i] == arr[i + count]) {
+                count++;
+            }
+
+            if(count > 1) {
+                System.out.print("[" + arr[i] + "'," + count + "], ");
+            }
+            i += count;
+        }
+    }
 }
