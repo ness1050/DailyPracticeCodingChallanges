72ab47b5b0773f2104491ef587f2120a7ba2bbbe
Naseem Qurbanli <nq222af@student.lnu.se>
2026-04-05 17:14:16 +0200

    added labeling and coloring

--- Tiktaktoe.java
+++ Tiktaktoe.java
@@ -1,6 +1,8 @@
 import java.awt.*;
 import javax.swing.*;
 
+import org.w3c.dom.Text;
+
 public class Tiktaktoe {
     int boardWidth = 600;
     int boardHeight = 650;
@@ -11,6 +13,9 @@ public class Tiktaktoe {
     }
 
     JFrame frame = new  JFrame();
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+
 
     Tiktaktoe() {
         frame.setVisible(true);
@@ -19,5 +24,18 @@ public class Tiktaktoe {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
+
+        textLabel.setBackground(Color.darkGray);
+        textLabel.setForeground(Color.white);
+        textLabel.setFont(new Font("Arial", Font.BOLD, 50));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Tic Tac Toe");
+        textLabel.setOpaque(true);
+
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textLabel);
+
     }
 }
