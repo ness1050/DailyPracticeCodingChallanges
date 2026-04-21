21bf6cb48a8ee5fb2af444c573b1bf8f06e87d8d
Naseem Qurbanli <nq222af@student.lnu.se>
2026-04-21 23:23:50 +0200

    check primes numbers in range of an integer

--- Problems/Patterns/primeNumber.java
+++ Problems/Patterns/primeNumber.java
@@ -1,16 +1,54 @@
 package Problems.Patterns;
 
+import java.util.Scanner;
+
 public class primeNumber {
     
     public static void main(String[] args) {
-        isPrime(15);
+        primeNumber p = new primeNumber();
+        isPrime(10);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int  a = scan.nextInt();
+        printNumbers(a);
+
     }
 
     public static boolean isPrime(int a) {
 
-       
+
         boolean result = a % 2 == 0 ?  true : false;
         System.out.println(result);
         return result;
     }
+
+    // we want to create a funciton
+    // that exacutes the function to find the numbers in between the given integer to check
+    // which numbers can prime 
+
+    public static boolean numbersOfPrime(int a ) {
+
+        if (a <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= a; i++) {
+            if (a % i == 0) {
+              return false;
+            }
+           
+        };
+
+        return true;
+    }
+
+    public static void printNumbers(int a) {
+        for (int i = 1; i <= a; i++) {
+            if (numbersOfPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+  
 }
---
+++ Problems/Strings/longestSubstring.java
@@ -0,0 +1,18 @@
+package Problems.Strings;
+
+public class longestSubstring {
+    
+
+    public static void main(String [] args) {
+
+    }
+
+    public String solution (String str) {
+         String name = "Naseem";
+        String eftername = "Qurbanali";
+
+        
+
+        return "";
+    }
+}
