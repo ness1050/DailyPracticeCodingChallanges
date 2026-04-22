28a10b1b79c904f59924caeb1869aa56978a35f6
Naseem Qurbanli <nq222af@student.lnu.se>
2026-04-22 23:58:20 +0200

    added half diamond patterns

--- Problems/Patterns/numberP.java
+++ Problems/Patterns/numberP.java
@@ -23,6 +23,8 @@ public class numberP {
         reversePrinting(action);
         decreasingPattern(action);
         increasingByOne(action);
+        System.out.println();
+        halfDiamond(action);
     }
 
     public static void reversePrinting(int n) {
@@ -50,6 +52,23 @@ public class numberP {
         for (int i = n; i >= 1; i--) {
             for (int j = n; j >= i; j--) {
                 System.out.print(j + "");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void halfDiamond(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j+ " ");
+            }
+            System.out.println();
+        }
+
+        for (int i = n -1 ; i >= 1; i--) {
+            for  (int j = 1; j <= i; j++) {
+                System.out.print(j+" ");
             }
             System.out.println();
         }
