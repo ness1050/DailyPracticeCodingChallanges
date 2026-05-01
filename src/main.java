6615449f73a92df084d2483c353e96b953b34ac2
Naseem Qurbanli <nq222af@student.lnu.se>
2026-05-01 13:09:56 +0200

    added some calculator functionility for practice

--- src/main.java
+++ src/main.java
@@ -9,28 +9,42 @@ public class main {
    
         calculator c = new calculator();
         Scanner scan = new Scanner(System.in);
-        
+    
         System.out.println("1.Addition " + "\n" + "2.Subtraction " + "\n" + "3.Multiplication " + "\n" + "4.Division" + "\n" + "Enter choice: ");
         int choice = scan.nextInt();
+        System.out.println("Enter a number: ");
+        int num1 = scan.nextInt();
+        System.out.println("Enter second number: ");
+        int num2 = scan.nextInt();
+
+      
 
         switch (choice) {
             case 1:
-                System.out.println("Enter a number: ");
-                int num1 = scan.nextInt();
-                System.out.println("Enter second number: ");
-                int num2 = scan.nextInt();
                 c.addition(num1, num2);
                 scan.close();
                 break;
-        
-            default:
+            case 2: 
+                c.Subtraction(num1, num2);
+                scan.close();
                 break;
+            case 3:
+                c.Multiplication(num1, num2);
+                scan.close();
+                break;
+            case 4:
+                c.division(num1, num2);
+                scan.close();
+                return;
+            default:
+                return;
+               
         }
     };
 
     
 
 
-
+    
 
 }
