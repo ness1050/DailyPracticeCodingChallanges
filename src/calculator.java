6bf93e8a268ff66bf000afb6480a89003891638a
Naseem Qurbanli <nq222af@student.lnu.se>
2026-05-02 14:03:02 +0200

    added pointer and decimal function

--- src/calculator.java
+++ src/calculator.java
@@ -23,9 +23,9 @@ public class Calculator {
     JPanel displayPanel = new JPanel();
     JPanel buttonPanel = new JPanel();
 
-    String [] buttonValues =  {
+    String[] buttonValues = {
         "AC", "+/-", "%", "÷", 
-        "7", "8", "9", "x",
+        "7", "8", "9", "×", 
         "4", "5", "6", "-",
         "1", "2", "3", "+",
         "0", ".", "√", "="
@@ -44,7 +44,7 @@ public class Calculator {
 
         displayLabel.setBackground(customBlack);
         displayLabel.setForeground(Color.white);
-        displayLabel.setFont(new Font("Arial", Font.PLAIN, 90));
+        displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));
         displayLabel.setHorizontalAlignment(JLabel.RIGHT);
         displayLabel.setText("0");
         displayLabel.setOpaque(true);
@@ -52,6 +52,51 @@ public class Calculator {
         displayPanel.setLayout(new BorderLayout());
         displayPanel.add(displayLabel);
         frame.add(displayPanel, BorderLayout.NORTH);
+
+        buttonPanel.setLayout(new GridLayout(5, 4));
+        buttonPanel.setBackground(customBlack);
+        frame.add(buttonPanel);
+
+        for (int i  = 0; i < buttonValues.length; i++) {
+        JButton button = new JButton();
+        String buttonValue = buttonValues[i];
+        button.setFont(new Font("Arial", Font.PLAIN, 35));
+        button.setText(buttonValue);
+        button.setFocusable(false);
+        buttonPanel.add(button);
+            if (Arrays.asList(topSymbols).contains(buttonValue)) {
+                button.setBackground(customLightGrey);
+                button.setForeground(customBlack);
+            } else {
+                button.setBackground(customDarkGrey);
+                button.setForeground(customBlack);
+            }
+            buttonPanel.add(button);
+            button.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e ) {
+                JButton button = (JButton) e.getSource();
+                String buttonValue = button.getText();
+                if (Arrays.asList(rightSymbols). contains(buttonValue)) {
+
+                } else if (Arrays.asList(topSymbols).contains(buttonValue) ) {
+
+                } else {
+                    if (buttonValue == ".") {
+                        if (!displayLabel.getText().contains(buttonValue)) {
+                            displayLabel.setText(displayLabel.getText() + buttonValue);
+                        }
+
+                    } else if ("0123456789".contains(buttonValue)) {
+                        if (displayLabel.getText() == "0") {
+                            displayLabel.setText(buttonValue);
+                        } else {
+                            displayLabel.setText(displayLabel.getText() + buttonValue);
+                        }
+                    }
+                }
+                }
+            });
+        }
 
     }
 }
