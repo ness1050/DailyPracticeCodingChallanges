8e6b4bee825ea505d49a5c8a56db18e324113125
Naseem Qurbanli <nq222af@student.lnu.se>
2026-05-18 18:07:41 +0200

    Creating interface choice for users

---
+++ BankManagement/src/view/Userinterface.java
@@ -0,0 +1,59 @@
+package view;
+
+import java.util.Scanner;
+
+public class Userinterface {
+    
+    public Scanner scan;
+
+    public void showMainMenu() {
+        System.out.println("\n");
+        System.out.println("Welcome to Java bankingSystem");
+        System.out.println("--------------------------------");
+        System.out.println("1.Create new account");
+        System.out.println("2.Deposit Money");
+        System.out.println("3.Withdraw Money");
+        System.out.println("4.Transfer Money");
+        System.out.println("5.Check balance");
+        System.out.println("6.View Transaction History");
+        System.out.println("7.Exit");
+    }
+
+    public int getUserChoice() {
+        System.out.println("Enter your choice 1-7");
+        while (scan.hasNext()) {
+            scan.nextLine();            
+        }
+
+        int choice = scan.nextInt();
+        scan.close();
+        return choice;
+    }
+
+    public String getUserName() {
+        System.out.println("Enter your name: ");
+
+        return scan.nextLine();
+    }
+
+    public String getEmail() {
+        System.out.println("Enter your email: ");
+        return scan.nextLine();
+    }
+
+    public int getAccountType() {
+        System.out.println("Select Accout type");
+        System.out.println("1.Saving account");
+        System.out.println("2.Current accont");
+        System.out.println("Enter Choice: ");
+        return scan.nextInt();
+    }
+
+    public double getInitialDeposit() {
+        System.out.println("Enter Initial Deposit Amount : ");
+        return scan.nextDouble();
+    }
+
+    
+
+}
